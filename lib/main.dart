f1bd413b1e0b2f6c067455552810c2c30e89b6a2
GraceGH <47500172+GraceGH@users.noreply.github.com>
2023-12-10 22:15:53 +0900

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'LoginPage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'mainPage.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+//GetMaterialApp
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MapEasy',
+      home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return const MainPage();
+            } else {
+              return const LoginPage();
+            }
+          }),
+    );
+  }
+}
